dbc7fbffeb448ba83a46b444d99fa57fc678f521
soutecdigital <soutecdigital@gmail.com>
2026-02-22 21:16:08 -0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,8 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  // Do not generate production browser source maps (keeps bundles minified only)
   productionBrowserSourceMaps: false,
+  // Isso ajuda o Cloudflare a entender a exportação das rotas
+  output: 'standalone', 
 }
 
 module.exports = nextConfig
